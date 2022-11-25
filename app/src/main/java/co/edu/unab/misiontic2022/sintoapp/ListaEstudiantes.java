a4e2efb24789705080e3f6c0e9e63356c78b826c
Alexander-Montes <neider.lancheros@o365.unab.edu.co>
2022-11-25 17:51:39 -0500

    App visualmente completa.

--- app/src/main/java/co/edu/unab/misiontic2022/sintoapp/ListaEstudiantes.java
+++ app/src/main/java/co/edu/unab/misiontic2022/sintoapp/ListaEstudiantes.java
@@ -3,6 +3,8 @@ package co.edu.unab.misiontic2022.sintoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListaEstudiantes extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ListaEstudiantes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_estudiantes);
+        Button btnVolverLista = findViewById(R.id.btnVolverLista);
+
+        btnVolverLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
